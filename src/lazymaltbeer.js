0a7145063912435f445e6489b0fdf50ee6a501ef
Fabian Hoffmann <fabian.hoffmann@otto.de>
2015-02-14 20:43:51 +0100

    add fixture for complex testing

--- src/lazymaltbeer.js
+++ src/lazymaltbeer.js
@@ -5,7 +5,7 @@ var Lazymaltbeer = function() {
      * Lazy load images with img tag.
      *
      * @param imgPlaceholder the placeholder is a html node of any type (e.g. a span element).
-     *                       The node has to provide a data element src and alt.
+     *                       The node has to provide the data elements src and alt.
      */
     var lazyLoadImg = function(imgPlaceholder) {
         var img = document.createElement("img");
