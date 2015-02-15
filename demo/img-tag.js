622bd7129a9f612bfd5b0b9d459b0e4423cf7dce
Fabian Hoffmann <fabian.hoffmann@otto.de>
2015-02-15 14:36:34 +0100

    add image placeholder and improve demo

--- demo/img-tag.js
+++ demo/img-tag.js
@@ -1,11 +1,19 @@
 (function () {
 
     var lazymaltbeer = Lazymaltbeer();
+
     var btnLazyLoad = window.document.getElementById("btn-lazy-load");
     var imgToLoad = window.document.getElementById("lazy-load-img");
 
+    var btnLazyLoad1 = window.document.getElementById("btn-lazy-load-1");
+    var imgToLoad1 = window.document.getElementById("lazy-load-img-1");
+
     btnLazyLoad.onclick = function() {
         lazymaltbeer.lazyLoadImg(imgToLoad);
+    };
+
+    btnLazyLoad1.onclick = function() {
+        lazymaltbeer.lazyLoadImg(imgToLoad1);
     };
 
 })();
