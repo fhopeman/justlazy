46cba08c8ef88de1af4d9f6d32930cafdf7b0b9f
Micha Regel <micha.regel@ottogroup.com>
2015-02-18 17:18:52 +0100

    suppurt for ie8

--- javascript/demo.js
+++ javascript/demo.js
@@ -1,23 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-
+function init() {
     var initLazyBtn = function (btn, lazyPlaceholderId) {
-        btn.onclick = function() {
+        btn.onclick = function () {
             var lazyPlaceholder = document.getElementById(lazyPlaceholderId);
             if (lazyPlaceholder) {
                 Justlazy.lazyLoadImg(lazyPlaceholder);
             }
-            
             return false;
         };
     };
-
     // img 1
     initLazyBtn(document.getElementById("loadImg1"), "lazyImg1");
-
     // img 2
     initLazyBtn(document.getElementById("loadImg2"), "lazyImg2");
-
     // img 3
     initLazyBtn(document.getElementById("loadImg3"), "lazyImg3");
+}
 
-});
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", init, false);
+}
+else {
+    document.attachEvent("DOMContentLoaded", init);
+}
