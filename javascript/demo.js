0cf7a014e8cb75f21601e6bc6ed446604a66ed51
Fabian Hoffmann <fabianhoffmann_@web.de>
2015-07-03 17:38:59 +0200

    refactor init functions

--- javascript/demo.js
+++ javascript/demo.js
@@ -1,17 +1,29 @@
 (function init() {
-    var initLazyBtn = function (btn, lazyPlaceholderId) {
-        btn.onclick = function () {
-            var lazyPlaceholder = document.getElementById(lazyPlaceholderId);
-            if (lazyPlaceholder) {
-                Justlazy.lazyLoadImg(lazyPlaceholder);
-            }
-            return false;
-        };
+
+    // example 1
+    document.getElementById("loadImg1").onclick = function() {
+        var imgPlaceholder = document.getElementById("lazyImg1");
+        Justlazy.lazyLoadImg(imgPlaceholder);
+
+        return false;
     };
-    // img 1
-    initLazyBtn(document.getElementById("loadImg1"), "lazyImg1");
-    // img 2
-    initLazyBtn(document.getElementById("loadImg2"), "lazyImg2");
-    // img 3
-    initLazyBtn(document.getElementById("loadImg3"), "lazyImg3");
+
+    // example 2
+    document.getElementById("loadImg2").onclick = function() {
+        var imgPlaceholder = document.getElementById("lazyImg2");
+        Justlazy.lazyLoadImg(imgPlaceholder);
+
+        return false;
+    };
+
+    // example 3
+    document.getElementById("loadImg3").onclick = function() {
+        var imgPlaceholder = document.getElementById("lazyImg3");
+        Justlazy.lazyLoadImg(imgPlaceholder);
+
+        return false;
+    };
+
+    // Justlazy.registerLazyLoad(document.getElementById("lazyImg4"));
+
 })();
