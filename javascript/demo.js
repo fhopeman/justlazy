23bc7008705b3ca700962d55215ba7c4c0809b4d
Fabian Hoffmann <fabianhoffmann_@web.de>
2015-07-03 17:47:44 +0200

    change ids of images and buttons

--- javascript/demo.js
+++ javascript/demo.js
@@ -1,24 +1,24 @@
 (function init() {
 
     // example 1
-    document.getElementById("loadImg1").onclick = function() {
-        var imgPlaceholder = document.getElementById("lazyImg1");
+    document.getElementById("loadImg1Btn").onclick = function() {
+        var imgPlaceholder = document.getElementById("img1");
         Justlazy.lazyLoadImg(imgPlaceholder);
 
         return false;
     };
 
     // example 2
-    document.getElementById("loadImg2").onclick = function() {
-        var imgPlaceholder = document.getElementById("lazyImg2");
+    document.getElementById("loadImg2Btn").onclick = function() {
+        var imgPlaceholder = document.getElementById("img2");
         Justlazy.lazyLoadImg(imgPlaceholder);
 
         return false;
     };
 
     // example 3
-    document.getElementById("loadImg3").onclick = function() {
-        var imgPlaceholder = document.getElementById("lazyImg3");
+    document.getElementById("loadImg3Btn").onclick = function() {
+        var imgPlaceholder = document.getElementById("img3");
         Justlazy.lazyLoadImg(imgPlaceholder);
 
         return false;
