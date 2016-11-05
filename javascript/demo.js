448791d4deee40f68a41cf5430341d279c2d6cdb
Fabian Hoffmann <fabianhoffmann_@web.de>
2016-11-05 14:36:47 +0100

    remove placeholder after loading

--- javascript/demo.js
+++ javascript/demo.js
@@ -1,16 +1,19 @@
 (function init() {
 
+    var removeJustlazySpinner = function() {
+        this.parentNode.classList.remove("justlazy-spinner");
+    };
+
     // example 1
     Justlazy.registerLazyLoadByClass("load-if-visible-placeholder", {
-        onloadCallback: function() {
-            this.parentNode.classList.remove("justlazy-spinner");
-        }
+        onloadCallback: removeJustlazySpinner
     });
 
     // example 2
     var placeholdersExample2 = document.querySelectorAll('.load-with-threshold-placeholder');
     for (var i = 0; i < placeholdersExample2.length; ++i) {
         Justlazy.registerLazyLoad(placeholdersExample2[i], {
+            onloadCallback: removeJustlazySpinner,
             threshold: 300
         });
     }
@@ -18,7 +21,9 @@
     // example 3
     document.getElementById("custom-event-btn").onclick = function() {
         var imgPlaceholder = document.getElementById("custom-event-placeholder");
-        Justlazy.lazyLoad(imgPlaceholder);
+        Justlazy.lazyLoad(imgPlaceholder, {
+            onloadCallback: removeJustlazySpinner
+        });
 
         return false;
     };
@@ -26,7 +31,9 @@
     // example 4
     document.getElementById("srcset-btn").onclick = function() {
         var imgPlaceholder = document.getElementById("srcset-placeholder");
-        Justlazy.lazyLoad(imgPlaceholder);
+        Justlazy.lazyLoad(imgPlaceholder, {
+            onloadCallback: removeJustlazySpinner
+        });
 
         return false;
     };
