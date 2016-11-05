d81f71984e99dc27e2c7f63c86f04b3f401b72cc
Fabian Hoffmann <fabianhoffmann_@web.de>
2016-11-05 14:29:16 +0100

    change spinner behaviour

--- javascript/demo.js
+++ javascript/demo.js
@@ -1,17 +1,14 @@
 (function init() {
 
     // example 1
-    Justlazy.registerLazyLoadByClass("load-if-visible", {
-        onreplaceCallback: function() {
-            this.classList.add("justlazy-placeholder");
-        },
+    Justlazy.registerLazyLoadByClass("load-if-visible-placeholder", {
         onloadCallback: function() {
-            this.classList.remove("justlazy-placeholder");
+            this.parentNode.classList.remove("justlazy-spinner");
         }
     });
 
     // example 2
-    var placeholdersExample2 = document.querySelectorAll('.with-threshold');
+    var placeholdersExample2 = document.querySelectorAll('.load-with-threshold-placeholder');
     for (var i = 0; i < placeholdersExample2.length; ++i) {
         Justlazy.registerLazyLoad(placeholdersExample2[i], {
             threshold: 300
@@ -19,16 +16,16 @@
     }
 
     // example 3
-    document.getElementById("btn-load-1").onclick = function() {
-        var imgPlaceholder = document.getElementById("img-1");
+    document.getElementById("custom-event-btn").onclick = function() {
+        var imgPlaceholder = document.getElementById("custom-event-placeholder");
         Justlazy.lazyLoad(imgPlaceholder);
 
         return false;
     };
 
     // example 4
-    document.getElementById("btn-load-2").onclick = function() {
-        var imgPlaceholder = document.getElementById("img-2");
+    document.getElementById("srcset-btn").onclick = function() {
+        var imgPlaceholder = document.getElementById("srcset-placeholder");
         Justlazy.lazyLoad(imgPlaceholder);
 
         return false;
